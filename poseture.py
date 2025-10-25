5bfe658d7a065d9adbb6004d3c786b3343966347
elperson <elpersonpl@gmail.com>
2025-10-25 18:39:00 +0100

    whatevur

--- poseture.py
+++ poseture.py
@@ -60,32 +60,6 @@ def main2():
   cap.release()
   cv2.destroyAllWindows()
 
-def tutorial_onlystillpicture():
-    # STEP 2: Create an PoseLandmarker object.
-    base_options = python.BaseOptions(model_asset_path='lib/pose_landmarker.task')
-    options = vision.PoseLandmarkerOptions(
-        base_options=base_options,
-        output_segmentation_masks=True)
-    detector = vision.PoseLandmarker.create_from_options(options)
-
-    # STEP 3: Load the input image.
-    image = mp.Image.create_from_file("lib/pic/girl.jpg")
-
-    # STEP 4: Detect pose landmarks from the input image.
-    detection_result = detector.detect(image)
-
-    # STEP 5: Process the detection result. In this case, visualize it.
-    annotated_image = draw_landmarks_on_image(image.numpy_view(), detection_result)
-    cv2.imshow("hi", cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR))
-    cv2.waitKey(0)
-
-    segmentation_mask = detection_result.segmentation_masks[0].numpy_view()
-    visualized_mask = np.repeat(segmentation_mask[:, :, np.newaxis], 3, axis=2) * 255
-    cv2.imshow("hi", visualized_mask)
-    cv2.waitKey(0)
-    # img = cv2.imread("girl.jpg")
-    # cv2.imshow("Image", img)
-    # cv2.waitKey(0)
 
 def draw_landmarks_on_image(rgb_image, detection_result):
   pose_landmarks_list = detection_result.pose_landmarks
